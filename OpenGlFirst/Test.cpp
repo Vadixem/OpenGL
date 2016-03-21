2c9eceac88a29ecbfd63a340a19efea44c8f0b62
Vadixem <vadixem@meta.ua>
2016-03-21 13:56:22 +0430

    I've got a square... some magic goes on

--- OpenGlFirst/Test.cpp
+++ OpenGlFirst/Test.cpp
@@ -95,12 +95,24 @@ int main()
     glDeleteShader(fragmentShader);
 
 
-    // Set up vertex data (and buffer(s)) and attribute pointers
+    // Set up vertex data (and buffer(s)) and attribute pointers for a square (2 triangles).
     GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f, // Left  
-         0.5f, -0.5f, 0.0f, // Right 
-         0.0f,  0.5f, 0.0f  // Top   
-    };
+        -0.5f, 0.5f, 0.0f,    // Top Left  
+         0.5f, 0.5f, 0.0f,    // Top Right 
+         0.5f,  -0.5f, 0.0f,  // Bot Right
+		-0.5f, -0.5f, 0.0f	  // Bot left
+	};
+
+	GLuint indices[] = 
+	{ // Note that we start from 0
+		0, 1, 2, // first tr
+		0, 2, 3  // second tr
+	};
+
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+
+
     GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -108,6 +120,12 @@ int main()
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	
+	// for square
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
@@ -128,18 +146,21 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw our first triangle
+        // Draw square
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
 
         // Swap the screen buffers
+		//
         glfwSwapBuffers(window);
     }
     // Properly de-allocate all resources once they've outlived their purpose
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
     return 0;
