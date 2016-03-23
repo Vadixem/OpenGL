5bead2966bc9ccf343c0c49c299094cf54262538
Vadixem <vadixem@meta.ua>
2016-03-23 13:35:32 +0430

    Draws 2 triangles

--- OpenGlFirst/Test.cpp
+++ OpenGlFirst/Test.cpp
@@ -25,21 +25,24 @@ const GLchar* fragmentShaderSource = "#version 330 core\n"
     "out vec4 color;\n"
     "void main()\n"
     "{\n"
-    "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "color = vec4(0.02f, 0.2f, 0.7f, 1.0f);\n"
     "}\n\0";
 
+
+/* DRAW 2 TRIANGLES */
 // The MAIN function, from here we start the application and run the game loop
 int main()
 {
-    // Init GLFW
+
+	// Init GLFW
     glfwInit();
     // Set all the required options for GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-    // Create a GLFWwindow object that we can use for GLFW's functions
+	
+	// Create a GLFWwindow object that we can use for GLFW's functions
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
     glfwMakeContextCurrent(window);
 
@@ -54,6 +57,9 @@ int main()
     // Define the viewport dimensions
     glViewport(0, 0, WIDTH, HEIGHT);
 
+	
+	// Wireframe mode
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // Build and compile our shader program
     // Vertex shader
@@ -61,6 +67,7 @@ int main()
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
     // Check for compile time errors
+
     GLint success;
     GLchar infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -95,36 +102,25 @@ int main()
     glDeleteShader(fragmentShader);
 
 
-    // Set up vertex data (and buffer(s)) and attribute pointers for a square (2 triangles).
+    // Set up vertex data (and buffer(s)) and attribute pointers for two triangles.
     GLfloat vertices[] = {
-        -0.5f, 0.5f, 0.0f,    // Top Left  
+        // first triangle
+		-0.5f, 0.5f, 0.0f,    // Top Left
+		0.0f, 0.5f, 0.0f,  // Top Centre
+		-0.25f, -0.5f, 0.0f,	  // Bot left
+
+		// second triangle
          0.5f, 0.5f, 0.0f,    // Top Right 
-         0.5f,  -0.5f, 0.0f,  // Bot Right
-		-0.5f, -0.5f, 0.0f	  // Bot left
+         0.0f, 0.5f, 0.0f,  // Top Centre
+		 0.25f, -0.5f, 0.0f  // Bot Right
 	};
-
-	GLuint indices[] = 
-	{ // Note that we start from 0
-		0, 1, 2, // first tr
-		0, 2, 3  // second tr
-	};
-
-	GLuint EBO;
-	glGenBuffers(1, &EBO);
-
 
     GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
     glBindVertexArray(VAO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	
-	// for square
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
@@ -143,14 +139,18 @@ int main()
 
         // Render
         // Clear the colorbuffer
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.7f, 0.7f, 0.6f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Draw square
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		// glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// draw first tr
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// draw second tr
+		glDrawArrays(GL_TRIANGLES, 3, 3);
+
         glBindVertexArray(0);
 
         // Swap the screen buffers
@@ -160,7 +160,6 @@ int main()
     // Properly de-allocate all resources once they've outlived their purpose
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
     return 0;
