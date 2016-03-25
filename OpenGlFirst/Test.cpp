e9bc31435a19676217a61703a7b82f8f2a910c01
Vadixem <vadixem@meta.ua>
2016-03-25 14:21:29 +0430

    Downloaded sources from guide triangle looks more legit

--- OpenGlFirst/Test.cpp
+++ OpenGlFirst/Test.cpp
@@ -8,8 +8,7 @@
 #include <GLFW/glfw3.h>
 
 
-// Function prototypes  
-
+// Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 // Window dimensions
@@ -17,50 +16,34 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 
 // Shaders
 const GLchar* vertexShaderSource = "#version 330 core\n"
-	// The position var has attrib position 0
-
     "layout (location = 0) in vec3 position;\n"
-	// Specify a colour output to the fragment shader	
-    "out vec4 vertexColor;"
-	"void main()\n"
-	"{\n"
-	// See how we directly give a vec3 to vec4's constructor
-    "gl_Position = vec4(position, 1.0f);\n"
-	"float r, g, b;\n"
-	"r = (gl_Position.x + gl_Position.y + 0.3) /2 ;\n"
-	"g = (-gl_Position.x + gl_Position.y + 0.3) /2 ;\n"
-	"b = (-gl_Position.y) + 0.6;\n"
-	// Set the out var to position
-	"vertexColor = vec4(r, g, b, 1.0f);"
-    "}\0";
-//
-const GLchar* fragmentShaderSource = "#version 330 core\n"
-	// The input var from the vert shader(same name and type)
-	"in vec4 vertexColor;\n"
-
-	"out vec4 color;\n"
-	// We set this var in OpenGL code.
-	"void main()\n"
+    "layout (location = 1) in vec3 color;\n"
+    "out vec3 ourColor;\n"
+    "void main()\n"
     "{\n"
-    "color = vertexColor;\n"
+    "gl_Position = vec4(position, 1.0);\n"
+    "ourColor = color;\n"
+    "}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+    "in vec3 ourColor;\n"
+    "out vec4 color;\n"
+    "void main()\n"
+    "{\n"
+    "color = vec4(ourColor, 1.0f);\n"
     "}\n\0";
 
-using namespace std;
-	
-/* DRAW 2 TRIANGLES */
 // The MAIN function, from here we start the application and run the game loop
 int main()
 {
-
-	// Init GLFW
+    // Init GLFW
     glfwInit();
     // Set all the required options for GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	
-	// Create a GLFWwindow object that we can use for GLFW's functions
+
+    // Create a GLFWwindow object that we can use for GLFW's functions
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
     glfwMakeContextCurrent(window);
 
@@ -75,18 +58,13 @@ int main()
     // Define the viewport dimensions
     glViewport(0, 0, WIDTH, HEIGHT);
 
-	
-	// Wireframe mode
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	
     // Build and compile our shader program
     // Vertex shader
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
     // Check for compile time errors
-
     GLint success;
     GLchar infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -113,61 +91,39 @@ int main()
     glLinkProgram(shaderProgram);
     // Check for linking errors
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) 
-	{
+    if (!success) {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
     }
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-	// ..:: UNIFORMS ::..
-	
 
-    // Set up vertex data (and buffer(s)) and attribute pointers for two triangles.
-    GLfloat vertices[] = 
-	{
-        // first triangle
-		-0.5f, 0.5f, 0.0f,    // Top Left
-		0.0f, 0.5f, 0.0f,  // Top Centre
-		-0.25f, -0.5f, 0.0f,	  // Bot left
-
-		// second triangle
-         0.5f, 0.5f, 0.0f,    // Top Right 
-         0.0f, 0.5f, 0.0f,  // Top Centre
-		 0.25f, -0.5f, 0.0f,  // Bot Right
-
-		// big triangle 
-		-1.0f, 1.0f, 0.0f,    // Top Left
-		 1.0f, 1.0f, 0.0f,    // Top Right 
-         0.0f, -1.0f, 0.0f	 // Bot Centre
-		
-	};
-
-
-	GLuint VBO1, VAO1;
-    glGenVertexArrays(1, &VAO1);
-    glGenBuffers(1, &VBO1);
-
+    // Set up vertex data (and buffer(s)) and attribute pointers
+    GLfloat vertices[] = {
+        // Positions         // Colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // Bottom Right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // Bottom Left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // Top 
+    };
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
     // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-    glBindVertexArray(VAO1);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glBindVertexArray(VAO);
 
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+    glBindVertexArray(0); // Unbind VAO
 
-    glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
-
-
-	
-	// How much vertex attribs can be used
-	GLint nrAttributes;
-	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-	cout << "Maximum nr of vertex attribs supported: " << nrAttributes << endl;
 
     // Game loop
     while (!glfwWindowShouldClose(window))
@@ -177,37 +133,25 @@ int main()
 
         // Render
         // Clear the colorbuffer
-        glClearColor(0.1f, 0.12f, 0.1f, 1.0f);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Draw the triangle
         glUseProgram(shaderProgram);
-	    glBindVertexArray(VAO1);
-
-		// draw first tr
-		// glDrawArrays(GL_TRIANGLES, 0, 3);
-		// draw second tr
-		// glDrawArrays(GL_TRIANGLES, 3, 3);
-		
-		glDrawArrays(GL_TRIANGLES, 6, 3);
-		
-		// Unbind first vert array
-		glBindVertexArray(0);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
 
         // Swap the screen buffers
-		//
         glfwSwapBuffers(window);
     }
     // Properly de-allocate all resources once they've outlived their purpose
-    glDeleteVertexArrays(1, &VAO1);
-    // 
-	glDeleteBuffers(1, &VBO1);
-    
-	// Terminate GLFW, clearing any resources allocated by GLFW.
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
-
     return 0;
 }
-
 
 // Is called whenever a key is pressed/released via GLFW
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
